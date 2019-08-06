b15ca340c7bf20247eff8347fd0280cccafa70d4
chipbaber <chipbaber@yahoo.com>
2019-08-06 15:48:51 -0400

    set cookietester to devmode.

--- js/cookietester.js
+++ js/cookietester.js
@@ -4,62 +4,62 @@
   */
 var devmode=true;
 var _version_number = '1.1';
-var win_location='http://launch.oracle.com/?ca_timline';
+var win_location='';
 var isEmployee =false;
 var emailaddress="";
-   
-function ReadCookie() {  
+
+function ReadCookie() {
    var allcookies = document.cookie;
    var hasSfCookie=false;
    var cookiename="none";
-   
+
 if (devmode) {//do nothing
 console.log('Accelerate Landing Pad Template is in Developer mode. ');
 }
 else {
    // Get all the cookies pairs in an array
    cookiearray  = allcookies.split(';');
-        
+
    // Now take key value pair out of this array
    for(var i=0; i<cookiearray.length; i++) {
-  
+
     cookiename = cookiearray[i].split('=')[0];
-    
+
      //check to see if WEB2000userid exists and extract email address
      if (cookiename.toLowerCase().indexOf("ora_ucm_info") >= 0) {
       emailaddress =cookiearray[i].split('~')[4];
       hasSfCookie=true;
       break;
       }
-   
+
     //check to see if SF_USERID exists and extract email address
     if (cookiename.toLowerCase().indexOf("sf_userid") >= 0) {
       emailaddress = cookiearray[i].split('=')[1];
       hasSfCookie=true;
       break;
       }
-      
+
     //check to see if WEB2000userid exists and extract email address
     if (cookiename.toLowerCase().indexOf("web2000userid") >= 0) {
       emailaddress = cookiearray[i].split('=')[1];
       hasSfCookie=true;
       break;
       }
-      
+
    }
-   
+
   if (!hasSfCookie) {
     //if no cookie then redirect
     console.log('No Cookie found so redirecting to core website.');
     window.location=win_location;
-   } 
+   }
   else {
     console.log('Inside ReadCookies for Landing Pad '+win_location);
     log('Viewing Landing Pad ',win_location);
     isOracleEmployee();
   }
-  
-  
+
+
  }
 }
 
@@ -75,15 +75,15 @@ function isOracleEmployee(){
  else{
     isEmployee =false;
  }
- 
+
 }
 
 
-/*function log sends actions from the webpage back to the server. 
+/*function log sends actions from the webpage back to the server.
  * */
 function log(operation,machine) {
 
-var p_url="http://isdportal.oracle.com/pls/portal/tsr_admin.CUSTOMIZE.sf_audit_click?p_code=C_CLICK&p_id1=";
+var p_url="your log rest point";
 var params = p_url+machine+"&p_id2=&p_user_layout="+operation;
 
 if (devmode){
@@ -101,6 +101,3 @@ http.send(null);
 }
 
 }
-
-
-
